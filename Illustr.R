095b77f04398e21a31aee0e29835b79e0aca0d20
NadimBLT <44828296+NadimBLT@users.noreply.github.com>
2018-12-13 13:20:03 +0100

    Update Illustr.R

--- Illustr.R
+++ Illustr.R
@@ -1,24 +1,20 @@
 path_repository="~/SBGM-master/"
 
-
-source(paste0(path_repository,"Functions-For-SepLogit.R"))
-source(paste0(path_repository,"SepLogit-Ref-DataShared.R"))
-source(paste0(path_repository,"SepLogit-Indep.R"))
-source(paste0(path_repository,"Functions_For_Fused.R"))
-source(paste0(path_repository,"SepLogit-Fused.R"))
-source(paste0(path_repository,"Fonctions_For_Guo.R"))
-source(paste0(path_repository,"Function_Guo.R"))
-
 #install.packages(paste0(path_repository,"FusedLasso_1.0.6.tar.gz"),repos=NULL,type="source")
 #install.packages(paste0(path_repository,"BMN_1.02.tar.gz"),repos=NULL,type="source")
-
-
 
 library(MatrixModels)
 library(glmnet)
 #library(FusedLasso)
 #library(BMN)
 
+source(paste0(path_repository,"Functions-For-SepLogit.R"))
+source(paste0(path_repository,"SepLogit-Ref-DataShared.R"))
+source(paste0(path_repository,"SepLogit-Indep.R"))
+#source(paste0(path_repository,"Functions_For_Fused.R"))
+#source(paste0(path_repository,"SepLogit-Fused.R"))
+#source(paste0(path_repository,"Fonctions_For_Guo.R"))
+#source(paste0(path_repository,"Function_Guo.R"))
 
 load(paste0(path_repository,"/M-3NN-R-0.25.Rdata"))
 Data=fichier[[2]]
@@ -26,9 +22,9 @@ head(Data)
 
 
 
-#RES_GUO          =Function_Guo(data=Data)
-#RES_FUSED        =SepLogit_Fused(data = Data,adap = FALSE,standardize = FALSE)
+
 RES_INDEP        =SepLogit_Indep(data = Data,adap = FALSE,standardize = FALSE)
 RES_REF          =SepLogit_Ref(data = Data,adap = FALSE,standardize = FALSE)
 RES_DataShared   =SepLogit_DataShared(data = Data,adap = FALSE,standardize = FALSE)
-
+#RES_FUSED        =SepLogit_Fused(data = Data,adap = FALSE,standardize = FALSE)
+#RES_GUO          =Function_Guo(data=Data)
